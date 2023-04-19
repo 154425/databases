dcab24900d7860a6a06ca26e6ee2fa50204f7f35
154425 <154425@csgaugustinus.nl>
2023-04-19 09:52:09 +0000

    cum

---
+++ databases/jolanda.sql
@@ -0,0 +1,14 @@
+create DATABASE 'jolanda';
+
+use 'jolanda';
+
+create table 'haarproducten'; ( 
+    'id' int auto_increment primary key,
+    'product' text not null,
+    'prijs' int not null
+);
+
+insert into 'haarproducten' ('id', 'product', 'prijs') values
+(1, 'shampoo', '15')
+(2, 'conditioner', '20')
+(3, 'zilvershampoo', '12');
